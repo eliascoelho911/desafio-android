1d8c3f5b9a4bfd26b2d0b0577b698eabf82d30cb
eliascoelho911 <elias_coelho911@hotmail.com>
2022-06-29 18:07:38 -0300

    test: Adiciona teste para LocalDataSource

---
+++ contacts/src/test/java/com/picpay/desafio/android/core/commons/data/LocalDataSourceTest.kt
@@ -0,0 +1,39 @@
+package com.picpay.desafio.android.core.commons.data
+
+import com.picpay.desafio.android.core.test.BaseTest
+import io.mockk.spyk
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class LocalDataSourceTest: BaseTest() {
+    private val localDataSource = spyk(LocalDataSource<String, String>())
+
+    @Test
+    fun shouldDataExists_whenGet_shouldReturnData() {
+        localDataSource.set(KEY, VALUE)
+
+        val result = localDataSource.get(KEY)
+
+        assertEquals(VALUE, result)
+    }
+
+    @Test
+    fun shouldDataNotExists_whenGet_shouldReturnNull() {
+        val result = localDataSource.get(KEY)
+
+        assertEquals(null, result)
+    }
+
+    @Test
+    fun shouldDataExists_whenRemove_shouldRemoveData() {
+        localDataSource.set(KEY, VALUE)
+        localDataSource.remove(KEY)
+
+        val result = localDataSource.get(KEY)
+
+        assertEquals(null, result)
+    }
+}
+
+private const val KEY = "Key"
+private const val VALUE = "Value"
--- core/core/src/main/java/com/picpay/desafio/android/core/commons/data/LocalDataSource.kt
+++ core/core/src/main/java/com/picpay/desafio/android/core/commons/data/LocalDataSource.kt
@@ -1,11 +1,5 @@
 package com.picpay.desafio.android.core.commons.data
 
-/**
- * Todo: Testar se get returna null quando não encontrar dado
- * Todo: Testar se get returna dado quando existir
- * Todo: Testar se set seta dado com sucesso
- * Todo: Testar se remove remove dado com sucesso
- */
 open class LocalDataSource<in Key : Any, T> {
     private val data: MutableMap<Key, T> = mutableMapOf()
 
